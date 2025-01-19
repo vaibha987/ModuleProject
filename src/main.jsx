efc9182b7ee4c672c1d826782d4dbe3f2fcf7f2f
vaibha987 <v2goyal95@gmail.com>
2025-01-19 10:46:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
-)
+);
